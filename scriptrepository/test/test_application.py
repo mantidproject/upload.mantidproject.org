b9ffdc547658feaad09e2fca1b0d438923f6e115
Martyn Gigg <martyn.gigg@gmail.com>
2015-07-20 22:19:08 +0100

    Replace wsgi_intercept with webtest

--- scriptrepository/test/test_application.py
+++ scriptrepository/test/test_application.py
@@ -4,9 +4,9 @@ import os
 import sys
 import unittest
 from urllib import urlencode
-import urllib2
-from wsgi_intercept import httplib2_intercept, add_wsgi_intercept
+from webtest import TestApp
 
+# Our application
 sys.path.append(os.path.join(os.path.dirname(__file__), "..", "application"))
 from server import application
 
@@ -14,16 +14,13 @@ from server import application
 HOST = 'localhost'
 PORT = 80
 URL = 'http://{0}:{1}/'.format(HOST, PORT)
+TEST_APP = None
 
 # ------------------------------------------------------------------------------
 def setUpModule():
-    def make_app():
-        return application
-    httplib2_intercept.install()
-    add_wsgi_intercept(HOST, PORT, make_app)
+    global TEST_APP
+    TEST_APP = TestApp(application)
 
-def tearDownModule():
-    httplib2_intercept.uninstall()
 # ------------------------------------------------------------------------------
 
 class ScriptUploadServerTest(unittest.TestCase):
@@ -31,46 +28,45 @@ class ScriptUploadServerTest(unittest.TestCase):
     # ---------------- Failure cases ---------------------
 
     def test_app_returns_405_for_non_POST_requests(self):
-        http = httplib2.Http()
-        resp, content = http.request(URL)
+        response = TEST_APP.get('/', status=405)
         expected_resp = {
-            'status': '405',
-            'content-length': '99',
-            'content-type': 'application/json; charset=utf-8'
+            'status': '405 Method Not Allowed',
+            'content-length': 99,
+            'content-type': 'application/json'
         }
-        self.check_response(expected=expected_resp, actual=resp)
+        self.check_response(expected=expected_resp, actual=response)
         self.check_replied_content(expected=dict(message='Endpoint is ready to accept form uploads.', detail='',
-                                                          pub_date='', shell=''), actual=content)
+                                                 pub_date='', shell=''), actual=response.body)
 
     def test_POST_of_form_without_all_information_produces_400_error(self):
-        http = httplib2.Http()
         data = dict(author='Joe Bloggs', mail='first.last@domain.com', comment='Test comment', path='muon')
-        resp, content = http.request(uri=URL, method='POST', body=urlencode(data))
+        response = TEST_APP.post('/', data,
+                                 status=400)
         expected_resp = {
-            'status': '400',
-            'content-length': '115',
-            'content-type': 'application/json; charset=utf-8'
+            'status': '400 Bad Request',
+            'content-length': 115,
+            'content-type': 'application/json'
         }
-        self.check_response(expected=expected_resp, actual=resp)
+        self.check_response(expected=expected_resp, actual=response)
         self.check_replied_content(expected=dict(message='Incomplete form information supplied.',
                                                  detail='Missing fields: file', pub_date='', shell=''),
-                                   actual=content)
+                                   actual=response.body)
 
     def test_POST_of_form_with_invalid_fields_produces_400_error(self):
-        http = httplib2.Http()
         data = dict(author='', mail='', comment='', path='')
-        resp, content = http.request(uri=URL, method='POST', body=urlencode(data))
+        response = TEST_APP.post('/', data,
+                                 status=400)
         expected_resp = {
-            'status': '400',
-            'content-length': '157',
-            'content-type': 'application/json; charset=utf-8'
+            'status': '400 Bad Request',
+            'content-length': 157,
+            'content-type': 'application/json'
         }
-        self.check_response(expected=expected_resp, actual=resp)
+        self.check_response(expected=expected_resp, actual=response)
         expected_content = dict(message='Incomplete form information supplied.',
                                 detail='Missing fields: file\nInvalid fields: author,mail,comment,path',
                                 pub_date='', shell='')
         self.check_replied_content(expected=expected_content,
-                                   actual=content)
+                                   actual=response.body)
 
     # def xtest_server_without_correct_environment_returns_500_error(self):
     #     http = httplib2.Http()
@@ -94,7 +90,9 @@ class ScriptUploadServerTest(unittest.TestCase):
     # -------------------------------------------------------------------------------------------
 
     def check_response(self, expected, actual):
-        self.assertEquals(expected, actual)
+        self.assertEquals(expected['status'], actual.status)
+        self.assertEquals(expected['content-type'], actual.content_type)
+        self.assertEquals(expected['content-length'], actual.content_length)
 
     def check_replied_content(self, expected, actual):
         self.assertEquals(json.dumps(expected), actual)
